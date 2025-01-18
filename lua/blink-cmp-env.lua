ca076beea25252fe5bf712995633c29d69147a68
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2025-01-18 22:15:09 +0800

    refactor: move global variables into setmetatable

--- lua/blink-cmp-env.lua
+++ lua/blink-cmp-env.lua
@@ -1,21 +1,10 @@
 --- @module 'blink.cmp'
 
---- @type boolean
-local cached_results = false
-
---- @type blink.cmp.CompletionItem[]
-local completion_items = {}
-
 --- @class blink-cmp-env.Options
 --- @field eval_on_confirm boolean
 --- @field item_kind uinteger
+--- @field show_braces boolean
 --- @field show_documentation_window boolean
-local opts = {
-	eval_on_confirm = false,
-	item_kind = require("blink.cmp.types").CompletionItemKind.Variable,
-	show_braces = false,
-	show_documentation_window = true,
-}
 
 --- @param value string[]
 local function setup_documentation_for_item(value)
@@ -25,39 +14,54 @@ local function setup_documentation_for_item(value)
 	}
 end
 
-local function setup_completion_items()
+--- @class EnvSource : blink.cmp.Source, blink-cmp-env.Options
+--- @field cached_results boolean
+--- @field completion_items blink.cmp.CompletionItem[]
+local env = {}
+
+--- @param opts blink-cmp-env.Options
+function env.new(opts)
+	--- @type blink-cmp-env.Options
+	local default_opts = {
+		eval_on_confirm = false,
+		item_kind = require("blink.cmp.types").CompletionItemKind.Variable,
+		show_braces = false,
+		show_documentation_window = true,
+	}
+
+	opts = vim.tbl_deep_extend(
+		"keep",
+		opts,
+		default_opts,
+		{ cached_results = false, completion_items = {} }
+	)
+
+	return setmetatable(opts, { __index = env })
+end
+
+function env:setup_completion_items()
 	-- Get a dictionary with environment variables and their respective values
 	local env_vars = vim.fn.environ()
 
 	for key, value in pairs(env_vars) do
 		-- Prepend $ to key, also surround in braces if `show_braces` is true
 		-- e.g. PATH -> $PATH -> ${PATH}
-		key = "$" .. (opts.show_braces and "{" .. key .. "}" or key)
+		key = "$" .. (self.show_braces and "{" .. key .. "}" or key)
 
 		-- Show documentation if `show_documentation_window` is true
 		local documentation = nil
-		if opts.show_documentation_window then
+		if self.show_documentation_window then
 			documentation = setup_documentation_for_item(value)
 		end
 
-		table.insert(completion_items, {
+		table.insert(self.completion_items, {
 			label = key,
 			-- Evaluate the environment variable if `eval_on_confirm` is true
-			insertText = opts.eval_on_confirm and value or key,
-			kind = opts.item_kind,
+			insertText = self.eval_on_confirm and value or key,
+			kind = self.item_kind,
 			documentation = documentation,
 		})
 	end
-end
-
---- @type blink.cmp.Source
-local env = {}
-
---- @param param_opts blink-cmp-env.Options
-function env.new(param_opts)
-	opts = vim.tbl_deep_extend("keep", param_opts, opts)
-
-	return setmetatable({}, { __index = env })
 end
 
 function env:get_completions(_, callback)
@@ -66,15 +70,15 @@ function env:get_completions(_, callback)
 	-- After the first time, there is no need to setup completion_items again
 	-- as the environments variables would unlikely be changed and the cached
 	-- completion_items is reused
-	if cached_results == false then
-		setup_completion_items()
-		cached_results = true
+	if self.cached_results == false then
+		self:setup_completion_items()
+		self.cached_results = true
 	end
 
 	callback({
 		is_incomplete_forward = false,
 		is_incomplete_backward = false,
-		items = completion_items,
+		items = self.completion_items,
 	})
 
 	return function() end
